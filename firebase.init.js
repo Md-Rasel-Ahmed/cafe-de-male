64ce6b453805a2217bcaf45a6cb316b898a3e0ea
Md-Rasel-Ahmed <nhd305812@gmail.com>
2026-01-03 21:35:14 +0500

    .env file create

--- firebase.init.js
+++ firebase.init.js
@@ -10,12 +10,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBEwGcNjKgJq-krXC4-xUkXUM0bdOFAoCg",
-  authDomain: "cafe-de-male.firebaseapp.com",
-  projectId: "cafe-de-male",
-  storageBucket: "cafe-de-male.firebasestorage.app",
-  messagingSenderId: "528726957140",
-  appId: "1:528726957140:web:3c4bb938a04e1e2e8821c1",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGIN_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
